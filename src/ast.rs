b9c8ce87234f4d9c60587745d133e8183707c10f
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-18 17:17:03 +0100

    add closure expression node

--- src/ast.rs
+++ src/ast.rs
@@ -2,7 +2,7 @@
 pub enum Expr {
     Val(Value), Var(String), Unary(Token, Box<Expr>),
     Binary(Box<Expr>, Token, Box<Expr>), Call(String, Vec<Expr>), CallValue(Box<Expr>, Vec<Expr>),
-    Array(Vec<Expr>), Field(Box<Expr>, String),
+    Array(Vec<Expr>), Field(Box<Expr>, String), Closure(Vec<String>, Vec<Stmt>),
     StructInit(String, Vec<(String, Expr)>),
     EnumInit(String, String, Option<Box<Expr>>),
 }
