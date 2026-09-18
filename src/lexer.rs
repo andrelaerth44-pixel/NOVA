6e204e33c563ee2709bf20cde0d07704cd4c8f09
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-18 17:24:35 +0100

    lex postfix try operator

--- src/lexer.rs
+++ src/lexer.rs
@@ -49,6 +49,7 @@ pub fn lex(src: &str) -> Result<Vec<Token>, String> {
             ';' => { out.push(Token::Semi); i+=1; }
             ':' => { out.push(Token::Colon); i+=1; }
             '.' => { out.push(Token::Dot); i+=1; }
+            '?' => { out.push(Token::Question); i+=1; }
             '!' => { if i+1<c.len() && c[i+1]=='=' {out.push(Token::Ne);i+=2} else {out.push(Token::Bang);i+=1} }
             '=' => { if i+1<c.len() && c[i+1]=='=' {out.push(Token::EqEq);i+=2} else {out.push(Token::Eq);i+=1} }
             '<' => { if i+1<c.len() && c[i+1]=='=' {out.push(Token::Le);i+=2} else {out.push(Token::Lt);i+=1} }
