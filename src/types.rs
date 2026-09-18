9268ded25b2272e542fb7bb1d2506292a0d5a4c4
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-18 16:02:02 +0100

    feat: expand NOVA primitive type system

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,25 @@
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub enum Type { Any, Number, Bool, String, Array(Box<Type>), Null, Function(Vec<Type>,Box<Type>), Unknown }
+pub enum Type {
+    Any, I32, I64, F32, F64, Number, Bool, String,
+    Array(Box<Type>), Null, Void,
+    Function(Vec<Type>, Box<Type>), Unknown,
+}
 impl Type {
-    pub fn name(&self)->String{match self{Type::Any=>"any".into(),Type::Number=>"number".into(),Type::Bool=>"bool".into(),Type::String=>"string".into(),Type::Array(t)=>format!("array<{}>",t.name()),Type::Null=>"null".into(),Type::Function(a,r)=>format!("fn({}) -> {}",a.iter().map(|x|x.name()).collect::<Vec<_>>().join(", "),r.name()),Type::Unknown=>"unknown".into()}}
-    pub fn compatible(&self,other:&Type)->bool{self==other||matches!(self,Type::Any|Type::Unknown)||matches!(other,Type::Any|Type::Unknown)}
+    pub fn name(&self)->String{
+        match self {
+            Type::Any=>"any".into(), Type::I32=>"i32".into(), Type::I64=>"i64".into(),
+            Type::F32=>"f32".into(), Type::F64=>"f64".into(), Type::Number=>"number".into(),
+            Type::Bool=>"bool".into(), Type::String=>"string".into(),
+            Type::Array(t)=>format!("{}[]",t.name()), Type::Null=>"null".into(), Type::Void=>"void".into(),
+            Type::Function(a,r)=>format!("fn({}) -> {}",a.iter().map(|x|x.name()).collect::<Vec<_>>().join(", "),r.name()),
+            Type::Unknown=>"unknown".into()
+        }
+    }
+    pub fn numeric(&self)->bool { matches!(self,Type::I32|Type::I64|Type::F32|Type::F64|Type::Number) }
+    pub fn compatible(&self,other:&Type)->bool {
+        self==other
+            || matches!(self,Type::Any|Type::Unknown)
+            || matches!(other,Type::Any|Type::Unknown)
+            || (self.numeric() && other.numeric())
+    }
 }
