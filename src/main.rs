b64a35415770d8cfe9dd3828b396e1b9ea00d31a
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-18 18:10:25 +0100

    update NOVA CLI version to 1.7.0

--- src/main.rs
+++ src/main.rs
@@ -39,10 +39,10 @@ fn load_program(path: &str) -> Result<Vec<Stmt>, String> {
 fn main(){
     let a:Vec<String>=env::args().collect();
     if a.len()<2 {
-        eprintln!("NOVA 1.6.0\nusage: nova run <file> | nova check <file> | nova ir <file> | nova build-c <file> [output.c] | nova build-native <file> [output] | nova app-check <file> | nova build-android <file> [MainActivity.kt] | nova version");
+        eprintln!("NOVA 1.7.0\nusage: nova run <file> | nova check <file> | nova ir <file> | nova build-c <file> [output.c] | nova build-native <file> [output] | nova app-check <file> | nova build-android <file> [MainActivity.kt] | nova version");
         return
     }
-    if a[1]=="version"{println!("NOVA 1.6.0");return}
+    if a[1]=="version"{println!("NOVA 1.7.0");return}
 
     if a[1]=="app-check" {
         let src=match fs::read_to_string(&a[2]){Ok(x)=>x,Err(e)=>{eprintln!("{}",e);std::process::exit(1)}};
