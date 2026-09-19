0808de953eb5c6250637150b5bdf0891a2c2156b
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-19 10:14:37 +0100

    type array push and pop

--- src/semantic.rs
+++ src/semantic.rs
@@ -369,6 +369,8 @@ impl Checker {
                     _ => {}
                 };
                 let builtin = match name.as_str() {
+                    "push" => Some((vec![crate::types::Type::Array(Box::new(crate::types::Type::Any)), crate::types::Type::Any], crate::types::Type::Array(Box::new(crate::types::Type::Any)))),
+                    "pop" => Some((vec![crate::types::Type::Array(Box::new(crate::types::Type::Any))], crate::types::Type::Generic("Option".into(), vec![crate::types::Type::Any]))),
                     "range" => Some((vec![crate::types::Type::Number, crate::types::Type::Number], crate::types::Type::Array(Box::new(crate::types::Type::Number)))),
                     "str" => Some((vec![crate::types::Type::Any], crate::types::Type::String)),
                     "len" => Some((vec![crate::types::Type::Any], crate::types::Type::Number)),
