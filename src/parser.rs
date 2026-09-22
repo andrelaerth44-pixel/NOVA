f00041b0d6903feb8ab008d0302cd79d4c1b4a90
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-22 19:38:47 +0100

    test: lock in nested conditional parser behavior

--- src/parser.rs
+++ src/parser.rs
@@ -239,3 +239,24 @@ impl Parser {
     }
 }
 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn parses_adjacent_nested_conditionals() {
+        let source = r#"
+            fn f(k) {
+                if k == \"let\" {
+                    value = 1
+                    return value
+                }
+                if k == \"assign\" {
+                    value = 2
+                    return value
+                }
+            }
+        "#;
+        Parser::new(crate::lex(source).expect(\"lex\")).program().expect(\"nested conditionals should parse\");
+    }
+}
