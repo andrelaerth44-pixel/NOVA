28b6c827334de67e7707c9af8aafec299693863a
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-22 19:32:23 +0100

    feat: support else-if chains in NOVA parser

--- src/parser.rs
+++ src/parser.rs
@@ -88,7 +88,7 @@ impl Parser {
             Token::Let=>{self.take();let n=match self.take(){Token::Ident(x)=>x,_=>return Err("expected identifier".into())};let ty=if self.eat(&Token::Colon){Some(self.type_name()?)}else{None};if !self.eat(&Token::Eq){return Err("expected =".into())}Ok(Stmt::Let(n,ty,self.expr()?))},
             Token::Print=>{self.take();Ok(Stmt::Print(self.expr()?))},
             Token::Return=>{self.take();Ok(Stmt::Return(self.expr()?))},
-            Token::If=>{self.take();let c=self.expr()?;let a=self.block()?;let b=if self.eat(&Token::Else){self.block()?}else{vec![]};Ok(Stmt::If(c,a,b))},
+            Token::If=>{self.take();let c=self.expr()?;let a=self.block()?;let b=if self.eat(&Token::Else){if *self.peek()==Token::If{vec![self.stmt()?]}else{self.block()?}}else{vec![]};Ok(Stmt::If(c,a,b))},
             Token::While=>{self.take();let c=self.expr()?;Ok(Stmt::While(c,self.block()?))},
             Token::For=>{self.take();let n=match self.take(){Token::Ident(x)=>x,_=>return Err("expected loop variable".into())};if !self.eat(&Token::In){return Err("expected in".into())}let it=self.expr()?;Ok(Stmt::For(n,it,self.block()?))},
             Token::Import=>{self.take();match self.take(){Token::Str(x)=>Ok(Stmt::Import(x)),_=>Err("import expects a string path".into())}},
